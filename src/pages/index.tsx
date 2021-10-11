44e31030394c443f56237300e4c428fde214f554
Renan Filgueiras Andrade <renanandrade@rubeus.com.br>
2021-10-11 20:18:14 -0300

    Trocada a funcionalidade de SSR para SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -38,10 +38,8 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1JjWkHKKojKQPVPNz2mlNCwW');
-
-  console.log(price);
 
   const product = {
     priceId: price.id,
@@ -54,6 +52,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
