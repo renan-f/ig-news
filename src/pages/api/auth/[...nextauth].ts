8c1f48ac23d256331dbfa0ecec6baab49d81e990
Renan Filgueiras Andrade <renanandrade@rubeus.com.br>
2021-10-17 00:10:44 -0300

    Tratando para não duplicar usuários ao fazer sigIn

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -6,14 +6,12 @@ import Providers from "next-auth/providers";
 import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
-    // Configure one or more authentication providers
     providers: [
         Providers.GitHub({
             clientId: process.env.GITHUB_ID,
             clientSecret: process.env.GITHUB_SECRET,
             scope: 'read:user'
         }),
-        // ...add more providers here
     ],
     callbacks: {
         async signIn(user, account, profile) {
@@ -21,9 +19,25 @@ export default NextAuth({
 
             try {
                 await fauna.query(
-                    q.Create(
-                        q.Collection('users'),
-                        { data: { email } }
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'),
+                            { data: { email } }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(user.email)
+                            )
+                        )
                     )
                 );
                 return true;
