d9c056c5708dd7e1c5ad71047c50e24d12fa9920
Renan Filgueiras Andrade <renanandrade@rubeus.com.br>
2021-10-30 13:33:06 -0300

    Adicionado pagamento com stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,16 +1,53 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
+import { fauna } from "../../services/fauna";
+import { query as q } from 'faunadb';
 import { stripe } from "../../services/stripe";
+
+type User = {
+    ref: {
+        id: string;
+    },
+    data: {
+        stripe_customer_id: string
+    }
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === "POST") {
         const session = await getSession({ req });
-        const stripeCustomer = await stripe.customers.create({
-            email: session.user.email,
 
-        })
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session.user.email)
+                )
+            )
+        )
+
+        let customerId = user.data.stripe_customer_id;
+        console.log('Customer: ', customerId)
+        if (!customerId) {
+            const stripeCustomer = await stripe.customers.create({
+                email: session.user.email,
+            })
+
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data: {
+                            stripe_customer_id: stripeCustomer.id,
+                        }
+                    }
+                )
+            )
+            customerId = stripeCustomer.id;
+        }
+
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
+            customer: customerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [
@@ -18,7 +55,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             ],
             mode: 'subscription',
             allow_promotion_codes: true,
-            success_url: process.env.STRIPE_SUCESS_URL,
+            success_url: process.env.STRIPE_SUCCESS_URL,
             cancel_url: process.env.STRIPE_CANCEL_URL
         })
 
