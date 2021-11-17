8cfaff5f0799bebc581ddacd6e5ecfdc00632034
Renan Filgueiras Andrade <renanandrade@rubeus.com.br>
2021-11-17 19:37:35 -0300

    Criação e estilização da pagina de posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head';
+import styles from './styles.module.scss';
+
+export default function Posts() {
+    return (<>
+        <Head>
+            <title>Posts | Ignews</title>
+        </Head>
+
+        <main className={styles.container}>
+            <div className={styles.posts}>
+                <a href="">
+                    <time>05 de novembro de 2021</time>
+                    <strong>Creating a Monorepo with Lerna & Yarn Workspace</strong>
+                    <p>In this guide, you will lean how to create a Monorepo to manage
+                        multiple packages with a sared build,
+                        test, and realease process.
+                    </p>
+                </a>
+                <a href="">
+                    <time>05 de novembro de 2021</time>
+                    <strong>Creating a Monorepo with Lerna & Yarn Workspace</strong>
+                    <p>In this guide, you will lean how to create a Monorepo to manage
+                        multiple packages with a sared build,
+                        test, and realease process.
+                    </p>
+                </a>
+                <a href="">
+                    <time>05 de novembro de 2021</time>
+                    <strong>Creating a Monorepo with Lerna & Yarn Workspace</strong>
+                    <p>In this guide, you will lean how to create a Monorepo to manage
+                        multiple packages with a sared build,
+                        test, and realease process.
+                    </p>
+                </a>
+            </div>
+        </main>
+    </>);
+}
