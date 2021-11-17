c86900c0a4be76ad578f2f1071c3d03a4edb524a
Renan Filgueiras Andrade <renanandrade@rubeus.com.br>
2021-11-17 20:43:24 -0300

    Criação de componente de link: ActiveLink

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,21 @@
-import Link from 'next/link';
 import { SignInButton } from '../SignInButton';
 
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
 export function Header() {
+
     return (
         <header className={styles.headerContainer}>
             <div className={styles.headerContent}>
 
                 <img src="/images/logo.svg" alt="ig.news" />
                 <nav>
-                    <Link href="/">
-                        <a className={styles.active} >Home</a>
-                    </Link>
-                    <Link href="/posts" prefetch>
+                    <ActiveLink href="/" activeClassName={styles.active}>
+                        <a>Home</a>
+                    </ActiveLink>
+                    <ActiveLink href="/posts" activeClassName={styles.active} prefetch>
                         <a>Post</a>
-                    </Link>
+                    </ActiveLink>
                 </nav>
 
                 <SignInButton />
