2ea5c3665858d0d1f1be32accc64de3df60f82fc
Renan Filgueiras Andrade <renanandrade@rubeus.com.br>
2021-11-29 20:07:31 -0300

    Adicionado tempo de revalidação da pagina estatica

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -84,6 +84,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
         props: {
             post
         },
-        redirect: 60 * 30 // 30 minutes
+        revalidate: 60 * 30 // 30 minutes
     }
 }
